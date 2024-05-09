051e53cb483d20de653814a4476218738e9145f3
Lupin <darshanbehere@gmail.com>
2024-05-09 15:30:35 +0530

    added test4

--- src/Test3.java
+++ src/Test3.java
@@ -2,4 +2,7 @@ public class Test3 {
     public static void main(String[] args) {
         System.out.println("Hello");
     }
+    public void print(){
+        System.out.println("Hello from print function");
+    }
 }
