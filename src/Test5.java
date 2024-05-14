a4bcded221ff47483ad855de69073bb22edd085d
Lupin <darshanbehere@gmail.com>
2024-05-14 10:22:24 +0530

    created print1 method

--- src/Test5.java
+++ src/Test5.java
@@ -2,4 +2,8 @@ public class Test5 {
     public static void main(String[] args) {
         System.out.println("test5 added5");
     }
+
+    public void print1(){
+        System.out.println("printing from print1");
+    }
 }
