204720246a02a62f6f5a7354ed11ca8f6167dfaf
Lupin <darshanbehere@gmail.com>
2024-05-24 19:39:23 +0530

    added print6

--- src/Test5.java
+++ src/Test5.java
@@ -10,12 +10,8 @@ public class Test5 {
         System.out.println("Hello from print3!");
     }
     public void print5(){
-<<<<<<< HEAD
         //Hello world
         System.out.println("Hello from print5!");
-=======
-        System.out.println("Hello from print5!!#");
->>>>>>> 49b093fdb881ffc4f347f4c3a09a4960a1a440a1
     }
     public void print6(){
         System.out.println("Hello from print 6!!!!!@");
