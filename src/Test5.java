39392b874caf07095b96bb019af6d8e398865af8
Lupin <darshanbehere@gmail.com>
2024-05-24 19:26:10 +0530

    added !! ehllo world

--- src/Test5.java
+++ src/Test5.java
@@ -10,6 +10,7 @@ public class Test5 {
         System.out.println("Hello from print3!");
     }
     public void print5(){
+        //Hello world
         System.out.println("Hello from print5!");
     }
     public void print6(){
